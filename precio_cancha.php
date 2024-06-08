d3c5305ef8f9968303446c0b72b997a410ca4fe4
Manuel Pedro <manuel.em.pedro@gmail.com>
2024-06-08 12:33:29 -0300

    Arreglado sistema de reconocimiento de nivel

--- precio_cancha.php
+++ precio_cancha.php
@@ -5,7 +5,7 @@
         header("Location:index.php");
     }
     
-    $cancha = intval($_POST["cancha_id"]);
+    $cancha_id = intval($_POST["cancha_id"]);
     $email = $_SESSION["email"];
     //Sirve para contar la cantidad de reservas que faltan para volver
     //a aplicar los beneficios
@@ -16,64 +16,83 @@
     
     include("./conexion.php");
     
-
     /*COMENTAR LA LINEA DE ABAJO DE ESTA PARA REALIZAR PRUEBAS CON LA RACHA Y DESCOMENTAR ESTA
     $reservas = mysqli_query($conexion, "SELECT racha, penalizacion
                             FROM clientes C");*/
 
-    $reservas = mysqli_query($conexion, "SELECT COUNT(*) racha, penalizacion
+    $query_reservas_cliente = mysqli_query($conexion, "SELECT COUNT(*) racha, penalizacion
                             FROM clientes C
                             JOIN reservas R on C.id_usuario = R.usuario_id");
-    $fila = mysqli_fetch_assoc($reservas);
+    $reservas_cliente = mysqli_fetch_assoc($query_reservas_cliente);
+    $racha = intval($reservas_cliente["racha"]);
+
+    //ACA BUSCO EL NIVEL
+    $query_nivel_usuario = mysqli_query($conexion, "SELECT DISTINCT N.id, N.cantidad_reservas, N.descuento, N.nombre FROM niveles N, niveles M 
+                                              WHERE M.cantidad_reservas >= N.cantidad_reservas 
+                                              AND $racha BETWEEN N.cantidad_reservas AND M.cantidad_reservas + N.cantidad_reservas 
+                                              ORDER BY N.id DESC 
+                                              LIMIT 1");
+    $nivel = mysqli_fetch_assoc($query_nivel_usuario);
+    
+    //Necesito saber si está en el nivel correspondiente. Debe tener más que ese nivel y menos que el siguiente.
+
 
     //FIJARSE SI ES DE NOCHE, AUMENTAR EL PRECIO DE LA CANCHA EN UN 20%    
-
-    if(intval($fila["penalizacion"]) != 0){
-        $beneficio = 1;
+    if(intval($reservas_cliente["penalizacion"]) != 0){
+        $beneficio = 0;
         //ACTUALIZAR EL CAMPO QUE SIRVE COMO CONTADOR RESTANDOLE 1
     }
-    else if(intval($fila["racha"]) >= 20)
-    {
-        $beneficio = 0.85;
+    else{
+        $beneficio = intval($nivel["descuento"]);
     }
-    else if(intval($fila["racha"]) >= 10)
-    {
-        $beneficio = 0.9;
-    }
-    else
-    {
-        $beneficio = 1;
-    }
+    // else if(intval($fila["racha"]) >= 20)
+    // {
+    //     $beneficio = 0.85;
+    // }
+    // else if(intval($fila["racha"]) >= 10)
+    // {
+    //     $beneficio = 0.9;
+    // }
+    // else
+    // {
+    //     $beneficio = 1;
+    // }
 
-    $beneficio_temp = $beneficio;
+    //$beneficio_temp = $beneficio;
 
     //Verificar el tema del beneficio, no da el precio justo pero da uno cercano a la hora de tener un horario a la noche con beneficio
-    if($hora == "20:00:00" || $hora == "21:00:00" || $hora == "22:00:00"){
-        $beneficio = $beneficio + 0.2;
-    }
-    else{
-        $beneficio = $beneficio_temp;
-    }
+    // if($hora == "20:00:00" || $hora == "21:00:00" || $hora == "22:00:00"){
+    //     $beneficio = $beneficio + 0.2;
+    // }
+    // else{
+    //     $beneficio = $beneficio_temp;
+    // }
 
-    $consulta = mysqli_query($conexion, "SELECT precio FROM canchas WHERE id = $cancha");
-    $fila = mysqli_fetch_assoc($consulta);
+    $query_canchas = mysqli_query($conexion, "SELECT precio FROM canchas WHERE id = $cancha_id");
+    $cancha = mysqli_fetch_assoc($query_canchas);
+    $precio = intval($cancha["precio"]);
+
+    if($hora == "20:00:00" || $hora == "21:00:00" || $hora == "22:00:00"){
+        $precio += $precio * 0.2;
+    }
 
     //Como la función que va a formatear a moneda el resultado está en el front,
     //tengo que separar el string en dos partes, una con el precio y otra con el descuento
     //porque si devolviera todo en un único string sería imposible formatear.
     //La función de formateo de php está deprecada, por eso uso la de JS.
     $respuesta = new stdClass();
-    $respuesta->precio = intval($fila["precio"]) * $beneficio;
-    if($beneficio == 0.85)
+    $respuesta->precio = $precio * (1 - $beneficio);
+    if($beneficio == 0.15)
         $respuesta->beneficio = "15% off";
-    else if($beneficio == 0.9)
-        $respuesta->beneficio = "10% off";
+    else if($beneficio == 0.20)
+        $respuesta->beneficio = "20% off";
     else
         $respuesta->beneficio = "";
 
-    mysqli_free_result($consulta);
+    mysqli_free_result($query_canchas);
+    mysqli_free_result($query_nivel_usuario);
+    mysqli_free_result($query_reservas_cliente);
     mysqli_close($conexion);
-
     echo json_encode($respuesta);
     
 
