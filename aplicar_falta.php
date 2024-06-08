cbe2deb38fdcece2fb463c55b543ddeb303b9249
Lucas <lucasrovella323@gmail.com>
2024-06-08 11:29:48 -0300

    Aplicar falta y baja de reserva

--- aplicar_falta.php
+++ aplicar_falta.php
@@ -8,31 +8,36 @@
    }
 
    $email = $_POST["email"];
-   $dni = $_POST["dni"];
    $id = $_POST["id_reserva"];
     //Sirve para contar la cantidad de reservas que faltan para volver
     //a aplicar los beneficios
 
    include("./conexion.php");
 
+   $conseguir_id = mysqli_query($conexion, "SELECT usuario_id FROM reservas
+                                 WHERE id = '$id'");
+
+   $consulta = mysqli_fetch_assoc($conseguir_id);
+   $id_cliente = $consulta['usuario_id'];
+
    //Le sumo la falta al usuario
-   $consulta = mysqli_query($conexion, "UPDATE clientes SET faltas = faltas + 1, racha = racha - 1 WHERE dni = '$dni'");
+   $consulta = mysqli_query($conexion, "UPDATE clientes SET faltas = faltas + 1, racha = racha - 1 WHERE id_usuario = '$id_cliente'");
    //Cambio la reserva a no asistida, para que no aparezca más en el panel del administrador
    $consulta = mysqli_query($conexion, "UPDATE reservas SET asistio = 0 WHERE id = $id");
    //Cuento las faltas del usuario
-   $consulta_faltas = mysqli_query($conexion, "SELECT faltas FROM clientes WHERE dni = '$dni'");
+   $consulta_faltas = mysqli_query($conexion, "SELECT faltas FROM clientes WHERE id_usuario = '$id_cliente'");
 
-   $consulta_asistencias = mysqli_query($conexion, "SELECT racha FROM clientes WHERE dni = '$dni'");
+   $consulta_asistencias = mysqli_query($conexion, "SELECT racha FROM clientes WHERE id_usuario = '$id_cliente'");
    $resultado_asistencias = intval(mysqli_fetch_assoc($consulta_asistencias)["racha"]);
 
    $resultado = intval(mysqli_fetch_assoc($consulta_faltas)["faltas"]);
    if($resultado > 3)
    {
       //Esta variable bloquea los beneficios por 3 reservas
-      $consulta = mysqli_query($conexion, "UPDATE clientes SET penalizacion = 3 WHERE dni = '$dni'");
+      $consulta = mysqli_query($conexion, "UPDATE clientes SET penalizacion = 3 WHERE id_usuario = '$id_cliente'");
 
       //Para evitar números negativos en contador de racha de asistencias
-      $consulta = mysqli_query($conexion, "UPDATE clientes SET faltas = 0, racha = 0 WHERE dni = '$dni'");
+      $consulta = mysqli_query($conexion, "UPDATE clientes SET faltas = 0 WHERE id_usuario = '$id_cliente'");
       
    }
 
--- baja_reserva.php
+++ baja_reserva.php
@@ -10,8 +10,14 @@
     $id = intval($_POST["id_reserva"]);
     $email = $_POST["email"];
 
+    $conseguir_id = mysqli_query($conexion, "SELECT usuario_id FROM reservas
+                                WHERE id = '$id'");
+
+    $consulta = mysqli_fetch_assoc($conseguir_id);
+    $id_cliente = $consulta['usuario_id'];
+
     $consulta = mysqli_query($conexion, "DELETE FROM reservas WHERE id = $id");
-    $consulta = mysqli_query($conexion, "UPDATE usuarios SET racha = racha - 1 WHERE email = '$email'");
+    $consulta = mysqli_query($conexion, "UPDATE clientes SET racha = racha - 1 WHERE id_usuario = '$id_cliente'");
 
     mysqli_free_result($consulta);
     mysqli_close($conexion);
