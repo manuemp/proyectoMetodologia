e2f7e7f77e29a4329afd4a5e1aa278ffd0e4ccc9
Lucas <lucasrovella323@gmail.com>
2024-06-09 23:28:54 -0300

    Cambio nav admin

--- nav_admin.php
+++ nav_admin.php
@@ -2,6 +2,9 @@
         <div class="nav_admin">
             <a href="./admin_reservas.php"><img class="logo" src="imgs/torinoLogoNegro_2.png" alt="logoTorino"></a>
             <div>
+                <a class="opcion" href="./admin_reservas.php">Administrar Reservas</a>
+                <a class="opcion" href="./admin_gestionar_faltas.php">Gestionar Faltas</a>
+                <a class="opcion" href="./canchas.php">Canchas</a>
                 <a class="opcion" href="./cerrar_sesion.php">Cerrar Sesión</a>
             </div>
         </div>
